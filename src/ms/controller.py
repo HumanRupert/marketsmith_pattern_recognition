02a33a31035bee49af33824008aecf2e9d5d860a
Ali Akhtari <aaliiaakhtarii@gmail.com>
2021-06-10 16:36:01 +0430

    [refactor] Inject dates and tickers to extract_n_store_cup_with_handles

--- src/ms/controller.py
+++ src/ms/controller.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import logging
+from typing import List
 
 import src.ms as ms
-from src.constants import TICKER, DATE_FORMAT
-from src.ms.utils import convert_defdate_to_timestamp, convert_csv_to_records
+from src.ms.utils import convert_csv_to_records
 from src.models import Constituent
-from src.ms.patterns import filter_cup_with_handles
+from src.ms.pattern import filter_cup_with_handles
+
+logging.basicConfig(level=logging.INFO)
 
 
 def extract_patterns(ticker: str, filter_method: callable, start: int, end: int, session=ms.AuthSession()):
@@ -39,7 +42,7 @@ def extract_patterns(ticker: str, filter_method: callable, start: int, end: int,
     return filtered_patterns
 
 
-def extract_n_store_patterns(ticker: str, filter_method: callable, start: str, end: str,  dataname: str, session=ms.AuthSession()):
+def extract_n_store_patterns(ticker: str, filter_method: callable, start: int, end: int,  dataname: str, session=ms.AuthSession()):
     """Extracts patterns from MS API and stores them in `data/` dir
 
     Parameters
@@ -50,22 +53,18 @@ def extract_n_store_patterns(ticker: str, filter_method: callable, start: str, e
     filter_method : `callable`
         method that filters target patterns from `GET_PATTERNS` endpoint response
 
-    start : `str`
-        start date in `DATE_FORMAT`
+    start : `int`
+        start date in millis
 
     end : `int`
-        end date in `DATE_FORMAT`
+        end date in millis
 
     dataname : `str`
-         name to use for storing t data
+         name to use for storing data
 
     session : `AuthSession`, optional
-        authenticated session, by default ms.AuthSession()
+        authenticated session, by default `ms.AuthSession()`
     """
-    # convert datw strings to millis
-    start, end = convert_defdate_to_timestamp(
-        start), convert_defdate_to_timestamp(end)
-
     # load and extract patterns
     patterns = extract_patterns(ticker, filter_method, start, end)
 
@@ -73,11 +72,28 @@ def extract_n_store_patterns(ticker: str, filter_method: callable, start: str, e
     ms.store_patterns(patterns, dataname, ticker)
 
 
-def extract_n_store_djia_cup_with_handles():
-    # read DJIA constituents
-    tickers = convert_csv_to_records("data/tickers.csv", Constituent)
+def extract_n_store_cup_with_handles(start: int, end: int, tickers: List[Constituent]):
+    """Loads tickers from `data/tickers.csv`, calls `extract_patterns` for each ticker to load Cup With Handle patterns, and then stores them in `data/patterns/ as CSV files
 
-    # extract and store patterns for tickers
-    for ticker in tickers:
-        extract_n_store_patterns(
-            ticker=ticker.symbol, filter_method=filter_cup_with_handles, start="2000-01-01", end="2020-04-01", dataname="cup_with_handles")
+    Parameters
+    ----------
+    start : `int`
+        start date in millis
+
+    end : `int`
+        end date in millis
+    """
+    for ix, ticker in enumerate(tickers):
+        logging.info(f"Fetching data for {ticker.symbol}")
+        logging.info(f"{ix}/{len(tickers)}")
+        patterns = extract_patterns(
+            ticker=ticker.symbol, filter_method=filter_cup_with_handles, start=start, end=end)
+        ms.store_patterns(patterns=patterns, ticker=ticker.symbol)
+
+        logging.info("––––––––––––––")
+
+
+if __name__ == "__main__":
+    tickers: List[Constituent] = convert_csv_to_records(
+        "data/tickers.csv", Constituent)
+    extract_n_store_cup_with_handles(1304411415000, 1619739000000, tickers)
