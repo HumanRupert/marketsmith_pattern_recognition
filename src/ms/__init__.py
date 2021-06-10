98a4d88314ee4123bed42e7ebaee8b860f5dbf27
Ali Akhtari <aaliiaakhtarii@gmail.com>
2021-06-10 16:35:08 +0430

    [fix] Fix imports in ms init

--- src/ms/__init__.py
+++ src/ms/__init__.py
@@ -1,5 +1,5 @@
 from .auth import AuthSession
 from .instrument import get_instrument
 from .user import get_user
-from .patterns import get_patterns, filter_cup_with_handles, store_patterns
+from .pattern import get_patterns, filter_cup_with_handles, store_patterns
 from .controller import extract_patterns, extract_n_store_patterns
